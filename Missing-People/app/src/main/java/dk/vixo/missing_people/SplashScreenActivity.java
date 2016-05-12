34b8520185972c7aeae0e759698d878be4f299b1
Martin Karlsen <1martinkarlsen@gmail.com>
2016-05-12 11:11:16 +0200

    With fragments

---
+++ Missing-People/app/src/main/java/dk/vixo/missing_people/SplashScreenActivity.java
@@ -0,0 +1,26 @@
+package dk.vixo.missing_people;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent splashIntent;
+        SharedPreferences userPref = getSharedPreferences("userPref", Context.MODE_PRIVATE);
+        if(userPref.getBoolean("isLoggedIn", false)) {
+            splashIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
+        } else {
+            splashIntent = new Intent(SplashScreenActivity.this, LoginActivity.class);
+        }
+
+        startActivity(splashIntent);
+        finish();
+    }
+}
