696ce2e42399d053d7ac2bc7a0f653392622d541
KSWorm <kasper@sylvestworm.dk>
2016-05-12 08:45:25 +0200

    Secret changed.

--- Missing_People_Web/src/java/rest/MissingRest.java
+++ Missing_People_Web/src/java/rest/MissingRest.java
@@ -8,6 +8,8 @@ import com.google.gson.JsonParser;
 import entity.Missing;
 import entity.Photo;
 import facade.MissingPeopleFacade;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.List;
 import javax.ws.rs.Consumes;
@@ -26,7 +28,7 @@ public class MissingRest {
 
     Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").setPrettyPrinting().create();
     MissingPeopleFacade mpFacade = new MissingPeopleFacade();
-    
+
     @Context
     private UriInfo context;
 
@@ -39,24 +41,24 @@ public class MissingRest {
     public Response getAllMissingPeople() {
         JsonObject response = new JsonObject();
         JsonArray jsonArr = new JsonArray();
-        
+
         List<Missing> missingList = mpFacade.getAll();
-        
-        for(Missing missingPerson : missingList) {
+
+        for (Missing missingPerson : missingList) {
             JsonObject singlePerson = new JsonObject();
-            
+
             singlePerson.addProperty("id", missingPerson.getId());
             singlePerson.addProperty("Name", missingPerson.getNameOfMissingPerson());
             singlePerson.addProperty("Description", missingPerson.getDescription());
             singlePerson.addProperty("DateOfMissing", missingPerson.getDateOfMissing().toString());
             singlePerson.addProperty("GeoPosition", missingPerson.getGeoPosition());
             singlePerson.addProperty("Photo", "");
-            
+
             jsonArr.add(singlePerson);
         }
         return Response.ok(gson.toJson(jsonArr), MediaType.APPLICATION_JSON).build();
     }
-    
+
     @POST
     @Produces("application/json")
     @Consumes("application/json")
@@ -64,13 +66,20 @@ public class MissingRest {
     public Response createNewSearch(String content) {
         System.out.println(content);
         JsonObject json = new JsonParser().parse(content).getAsJsonObject();
-        
+
         System.out.println(json);
         
         Photo photoOfMissingPerson = gson.fromJson(json.get("file"), Photo.class);
         Missing newSearch = gson.fromJson(content, Missing.class);
+
+        System.out.println("newSearch " + newSearch.getNameOfMissingPerson());
+        System.out.println("newSearch " + newSearch.getImage());
+
         newSearch.setImage(photoOfMissingPerson);
-        
+
+        System.out.println("newSearch " + newSearch.getNameOfMissingPerson());
+        System.out.println("newSearch " + newSearch.getImage());
+
         // return null;
         return Response.ok(gson.toJson(mpFacade.createSearch(newSearch)), MediaType.APPLICATION_JSON).build();
     }
--- Missing_People_Web/src/java/security/Secrets.java
+++ Missing_People_Web/src/java/security/Secrets.java
@@ -1,5 +1,6 @@
 package security;
 
 public class Secrets {
-    public static final String tokenSecret = "awdefgrklfes2adw82adwda";
+    //public static final String tokenSecret = "awdefgrklfes2adw82adwda";
+    public static final String tokenSecret = "DB4AEF4719809709E560ED8DE2F9C77B886B963B28BA20E9A8A621BBD4ABA400";
 }
