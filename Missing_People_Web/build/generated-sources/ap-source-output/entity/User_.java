483964b396ebb916c2e96a94ffd87489d83f8878
Martin Karlsen <1martinkarlsen@gmail.com>
2016-05-27 15:44:49 +0200

    Fejlretning på follow api

--- Missing_People_Web/build/generated-sources/ap-source-output/entity/User_.java
+++ Missing_People_Web/build/generated-sources/ap-source-output/entity/User_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-27T15:08:48")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-27T15:33:15")
 @StaticMetamodel(User.class)
 public class User_ { 
 
